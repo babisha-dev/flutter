439b91877c9adbf23a6579c5682afbb1b86b97de
babisha04 <babishariya@gmail.com>
2026-01-17 11:43:55 +0530

    Add Scaffold HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ void main(){
 }
 
 class MyApp extends StatelessWidget{
+ // const MyApp({super.key});
+
  
 
 
@@ -12,6 +14,42 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context){
   return MaterialApp(
     debugShowCheckedModeBanner: false,
+    title:'Flutter Practice',
+    theme: ThemeData(primarySwatch:Colors.blue),
+    home:HomePage(),
+
   );
+  
 }
+}
+
+class HomePage extends StatefulWidget{
+//  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+
+}
+class _HomePageState extends State<HomePage>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My First Scaffold'),
+
+      ),
+
+      body: Center(
+        child: Text('Home Screen', style: TextStyle(fontSize: 22),
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(onPressed: (){ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('FAB CLicked')),
+      );
+      },
+child: Icon(Icons.add),
+       ),
+    );
+  }
+
 }
