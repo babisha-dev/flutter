74892f7becb280acbed0d94e4e725edc7c65e507
babisha04 <babishariya@gmail.com>
2026-01-19 22:56:52 +0530

    Added drawer property

--- lib/main.dart
+++ lib/main.dart
@@ -49,6 +49,33 @@ class _HomePageState extends State<HomePage>{
       },
 child: Icon(Icons.add),
        ),
+        drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue),
+              child: Text(
+                'Menu',
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 
